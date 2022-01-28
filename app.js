c9b2be34d3adb10bab4cc45e186fea1216aa6c83
Venu <venu6696@gmail.com>
2022-01-28 14:53:17 +0530

    minor changes

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/user');
-const nodemailer = require('nodemailer');
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
 
+const port = process.env.PORT || 5000;
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(5000, console.log('server is listening on port 5000'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+    app.listen(port, console.log(`server is listening on port ${port}...`));
   } catch (err) {
     console.log(err);
   }
